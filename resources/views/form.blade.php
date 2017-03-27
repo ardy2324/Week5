f7f2e323e4395aa4784ea76d51b400b01243a42e
Ardy Stefanus <astefanus01@student.ciputra.ac.id>
2017-03-27 15:03:02 +0700

    Tugas Week5

---
+++ resources/views/form.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Forms</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <form action="" method="post" class="form-horizontal">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" name="nama" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="gender">Jenis Kelamin</label>
+                        <div class="radio">
+                            <label><input type="radio" name="gender" value="m">Laki-laki</label>
+                        </div>
+                        <div class="radio">
+                            <label><input type="radio" name="gender" value="f">Perempuan</label>
+                        </div>
+                        <div class="radio">
+                            <label><input type="radio" name="gender" value="o">Other</label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="weapon">Senjata Pilihan</label>
+                        <h5>(*Bisa pilih lebih dari satu)</h5>
+                        <input type="checkbox" name="weapon[]" value="Maramusa">Maramusa<br>
+                        <input type="checkbox" name="weapon[]" value="Exalibur">Exalibur<br>
+                        <input type="checkbox" name="weapon[]" value="Sange & Yashe">Sange & Yasha<br>
+                        <input type="checkbox" name="weapon[]" value="Chuirass">Chuirass
+                    </div>
+                    <button type="submit" class="btn btn-warning">Submit</button>
+                </form>
+
+                <br> @if(isset($nama)) Halo, {{ $nama }} @endif
+                <br> @if(isset($email)) Email kamu {{ $email }} @endif
+                <br> @if(isset($gender)) Kamu adalah seorang @if($gender == "m") Laki-laki @elseif($gender == "f") Perempuan @else Ga jelas @endif @endif
+                <br> @if(isset($weapon)) Senjata Pilihan:
+                     @foreach($weapon as $weap)
+                         <br> -> {{ ucwords($weap) }}
+                     @endforeach
+                     @endif
+            </div>
+        </div>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ()
+{
+    return view('form');
 });
+
+Route::post('/', 'FormController@index');
